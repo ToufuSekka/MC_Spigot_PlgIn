3919cd6825e4f2d637b6154e39eb522c99a8d1e3
冬風雪花 <47716286+ToufuSekka@users.noreply.github.com>
2021-12-15 10:15:34 +0900

    SQLSys, Dice 마이그래이션 완료
    마이그래이션 하면서 일부 예외를 개선함.

--- Publ_PlugInOut_Over1.18x/spigot/src/main/java/org/spigotmc/spigot/SysSpt/SQLSystem.java
+++ Publ_PlugInOut_Over1.18x/spigot/src/main/java/org/spigotmc/spigot/SysSpt/SQLSystem.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class SQLSystem {
 	
 	//That is Inly Test Server. Do not Delete it!
-	private static String SERVER= "jdbc:mysql://localhost:3306/mctotalsys";
+	private static String SERVER_local= "jdbc:mysql://localhost:3306/mctotalsys";
 	
 	//Tha is RealServer
 	//private static String SERVER= "jdbc:mysql://wint0719.codns.com:3306/mctotalsys?allowPublicKeyRetrieval=true&useSSL=false";
@@ -18,15 +18,68 @@ public class SQLSystem {
 	
 	public boolean Reserve(SQLCMD_Reserved Type, String[] Datas) {
 		boolean Checker = false;
-		this.Query="";
-		this.Query = Type.GetQuery();
+		this.Query=Type.GetQuery();
 		
+		INIT();
+		
+		try {
+			this.ppst = this.con.prepareStatement(Query);
+			
+			switch (Type) {
+			case Rigist:
+				this.ppst.setString(1, Datas[0]);//UUID
+				this.ppst.executeUpdate();
+				break;
+			case Login:
+				this.ppst.setString(1, Datas[0]);//UUID
+				this.ppst.executeUpdate();
+				Checker = true;
+				break;
+			case SearchUser:
+				this.ppst.setString(1, Datas[0]);//UUID
+				this.Res = this.ppst.executeQuery();
+				Checker = this.Res.first();
+				break;
+			case GameLeft:
+				this.ppst.setString(1, Datas[0]);//UUID
+				this.ppst.executeUpdate();
+				Checker = true;
+				break;
+				default :
+					Checker = false;
+				break;
+			}
+		}catch(SQLException SQLe) {
+			SQLe.printStackTrace();
+		}
+		CloseAll();
 		return Checker;
 	};
 	
 	private void INIT() {
-		
+		try {
+			//This is just TESTSERVER VERSION!
+			//DO NOT use for public mc_Server
+			this.con = DriverManager.getConnection(SERVER_local,"root",null);
+			
+			
+			//this.con = DriverManager.getConnection(SERVER,"mcplayer","vonrk08217");
+		}catch(SQLException SQLe){
+			SQLe.printStackTrace();
+		}
 	}
 	
-	private void CloseAll() {}
+	private void CloseAll() {
+		try {
+			if(!this.Res.isClosed())
+				this.Res.close();
+			if(!this.ppst.isClosed())
+				this.ppst.close();
+			if(!this.con.isClosed())
+				this.con.close();
+		
+		}catch(SQLException SQLe) {
+			// To Something?
+		}
+	}
 }
