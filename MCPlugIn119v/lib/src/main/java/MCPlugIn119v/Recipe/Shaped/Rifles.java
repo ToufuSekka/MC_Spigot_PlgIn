7c2ab8722dae2189f579f8925c58940e44a86956
冬風雪花 <47716286+ToufuSekka@users.noreply.github.com>
2022-04-01 17:00:06 +0900

    Create Rifles.java

---
+++ MCPlugIn119v/lib/src/main/java/MCPlugIn119v/Recipe/Shaped/Rifles.java
@@ -0,0 +1,17 @@
+package MCPlugIn119v.Recipe.Shaped;
+
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.plugin.java.*;
+
+public class Rifles {
+	
+	public Rifles(JavaPlugin plugins) {
+		Bukkit.addRecipe(null);
+	}
+	
+	private ShapedRecipe RifleType1() {
+		
+		return null;
+	}
+}
